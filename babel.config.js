cb33c9949eae7be9f6f214fda59da1a90400e77b
Dalila Hernandez <hernandezdalila018@gmail.com>
2025-07-01 15:45:53 -0500

    Upgrading version of async storage

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,10 @@ module.exports = function(api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ['module:react-native-dotenv']
+      ['module:react-native-dotenv'],
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods', { loose: true }],
+      ['@babel/plugin-transform-private-property-in-object', { loose: true }],
     ],
   };
 };
